19f59be62c7a116af6934e0271994a0f4d5f8a28
mares <lost_mares@hotmail.com>
2016-07-11 14:36:32 -0500

    Agregando cambios faltantes en el commit anterior, separacion de archivos

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-
-
 (function(){
 	
 	"use strict";
 
-	var app = angular.module('app', []);
-
-
-	app.controller('mainCtrl', function($scope, userService){
-		$scope.titulo = 'Hola mundo';
-		$scope.limpiar = function(){
-			$scope.titulo = 'Hola';
-		}
-	});
-
-
-	app.service('userService', function(){
-		this.users = [{
-			nobre: 'david',
-			edad: '15'
-		},
-		{
-			nombre: 'mariano',
-			edad: '27'
-		}];
-	});
-
-
-
-
+	angular.module('app', []);
 
 })();
 
